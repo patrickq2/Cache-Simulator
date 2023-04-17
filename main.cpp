a14d7c894116a4702921c7ad57d3886ab03c5ed8
Patrick Quinlan <patrickquinlan6@gmail.com>
2023-04-17 13:06:28 -0400

    fixed main file only need to output statistics for each cache method

--- main.cpp
+++ main.cpp
@@ -15,12 +15,12 @@ std::string hexToBin(const std::string& s)
     ss >> n;
     std::bitset<32> b(n);
 
-    return b.to_string().substr(32 - 4*(s.length()));
+    return b.to_string().substr(32 - 4 * (s.length()));
 }
 
 int main() {
     // gets input parameters for cache from user
-    int bytes; 
+    int bytes;
     std::cout << "How many bytes will the cache be? " << std::endl;
     std::cin >> bytes;
 
@@ -29,27 +29,45 @@ int main() {
     std::cin >> blockSize;
 
     int linesPerSet;
-    std::cout<< "How many lines per set? " << std::endl;
+    std::cout << "How many lines per set? " << std::endl;
     std::cin >> linesPerSet;
 
     //data for splitting binary to be stored
-    int numLines = bytes/blockSize;
+    int numLines = bytes / blockSize;
     int offsetWidth = std::log2(blockSize);
     int lineWidth = std::log2(numLines);
-    int setWidth = std::log2(numLines/linesPerSet);
+    int setWidth = std::log2(numLines / linesPerSet);
+    //direct variables
+    int directHit = 0;
+    int directMiss = 0;
+
+    //full associative variables
+    int fullHit = 0;
+    int fullMiss = 0;
+    int fullHitLRU = 0;
+    int fullMissLRU = 0;
+    int counter = 0;
+
+    //set associative variables
+    int setHit = 0;
+    int setMiss = 0;
+    int setHitLRU = 0;
+    int setMissLRU = 0;
 
     // data structures for cache implementation
     std::vector<std::tuple<int, int, int, char>> directMap(numLines);
-    std::vector<std::tuple<int, int, int, char>> setAssociative(numLines);
-    std::vector<std::tuple<int, int, char>> fullAssociative(numLines); 
+    std::vector<std::tuple<int, int, int, int>> setAssociativeLRU(numLines);
+    std::vector<std::tuple<int, int, int, char>> setAssociativeFIFO(numLines);
+    std::vector<std::tuple<int, int, int>> fullAssociativeLRU(numLines);
+    std::vector<std::tuple<int, int, char>> fullAssociativeFIFO(numLines);
 
     std::ifstream inputFile;
-    inputFile.open("TraceFiles/swim.trace");
+    inputFile.open("TraceFiles/gcc.trace");
     if (inputFile.is_open()) {
         std::string temp;
         while (std::getline(inputFile, temp)) {
 
-            temp = temp.substr(4,8);
+            temp = temp.substr(4, 8);
             std::string tempBin = hexToBin(temp);
 
             //direct map case
@@ -60,23 +78,129 @@ int main() {
             //converting tag to decimal for ease
             int tagDec = stoi(tag, 0, 2);
 
+            if (std::get<3>(directMap.at(stoi(line, 0, 2))) == 'F') {
+                if (std::get<0>(directMap.at(stoi(line, 0, 2))) == tagDec) {
+                    directHit++;
+                }
+                else {
+                    directMap.at(stoi(line, 0, 2)) = (std::tuple<int, int, int, char>(tagDec, stoi(offset), stoi(line), 'F'));
+                    directMiss++;
+                }
+            }
+            else {
+                directMap.at(stoi(line, 0, 2)) = (std::tuple<int, int, int, char>(tagDec, stoi(offset), stoi(line), 'F'));
+                directMiss++;
+            }
+
+
+
             //Fully associative case
-            tag =  tempBin.substr(0, tempBin.size() - offsetWidth);
+            tag = tempBin.substr(0, tempBin.size() - offsetWidth);
             offset = tempBin.substr(tag.size(), offsetWidth);
 
             //converting tag to decimal for ease
             tagDec = stoi(tag, 0, 2);
+            //FIFO full associative
+            int position = counter % numLines;
+            bool found = false;
+            for (int i = 0; i < fullAssociativeFIFO.size(); i++) {
+                if (std::get<0>(fullAssociativeFIFO.at(i)) == tagDec) {
+                    fullHit++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                fullAssociativeFIFO.at(position) = (std::tuple<int, int, char>(tagDec, stoi(offset), 'F'));
+                fullMiss++;
+            }
 
+
+            found = false;
+
+            //LRU full associative
+            for (int i = 0; i < fullAssociativeLRU.size(); i++) {
+                if (std::get<0>(fullAssociativeLRU.at(i)) == tagDec) {
+                    fullHitLRU++;
+                    std::get<2>(fullAssociativeLRU.at(i)) = counter;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                int min = 0;
+                for (int i = 0; i < fullAssociativeLRU.size(); i++) {
+                    if (std::get<2>(fullAssociativeLRU.at(i)) < std::get<2>(fullAssociativeLRU.at(min))) {
+                        min = i;
+                    }
+                }
+                fullAssociativeLRU.at(min) = (std::tuple<int, int, int>(tagDec, stoi(offset), counter));
+                fullMissLRU++;
+            }
+
+            found = false;
             //set associative case
             tag = tempBin.substr(0, tempBin.size() - setWidth - offsetWidth);
             std::string cacheSet = tempBin.substr(tag.size(), setWidth);
-            offset = tempBin.substr(tag.size() + cacheSet.size(), offsetWidth); 
+            offset = tempBin.substr(tag.size() + cacheSet.size(), offsetWidth);
 
             //converting tag to decimal for ease
             tagDec = stoi(tag, 0, 2);
+
+            position = ((stoi(cacheSet, 0, 2)) * linesPerSet);
+            int n = linesPerSet;
+            //FIFO case
+            for (int i = 0; i < n; i++) {
+                if (i + position >= setAssociativeFIFO.size()) {
+                    n -= i;
+                    i = 0;
+                }
+                if (std::get<0>(setAssociativeFIFO.at(i + position)) == tagDec) {
+                    setHit++;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                setAssociativeFIFO.at(position + (counter % linesPerSet)) = (std::tuple<int, int, int, char>(tagDec, stoi(offset), stoi(cacheSet), 'F'));
+                setMiss++;
+            }
+            found = false;
+            //LRU case
+            position = ((stoi(cacheSet, 0, 2)) * linesPerSet);
+            n = linesPerSet;
+            for (int i = 0; i < n; i++) {
+                if (i + position >= setAssociativeLRU.size()) {
+                    n -= i;
+                    i = 0;
+                }
+                if (std::get<0>(setAssociativeLRU.at(i + position)) == tagDec) {
+                    setHitLRU++;
+                    std::get<3>(setAssociativeLRU.at(i + position)) = counter;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                n = linesPerSet;
+                int min = position;
+                for (int i = 0; i < n; i++) {
+                    if (i + position >= setAssociativeLRU.size()) {
+                        n -= i;
+                        i = 0;
+                    }
+                    if (std::get<3>(setAssociativeLRU.at(i + position)) < std::get<3>(setAssociativeLRU.at(min))) {
+                        min = i + position;
+                    }
+                }
+                setAssociativeLRU.at(min) = (std::tuple<int, int, int, char>(tagDec, stoi(offset), stoi(cacheSet), counter));
+                setMissLRU++;
+            }
+            found = false;
+            counter++;
         }
-        inputFile.close();   
+        inputFile.close();
     }
-    
+
     return 0;
 }
